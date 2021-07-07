ff6d207b8e100d651d40e7b1b92a93aaef334942
enwes <86522364+enwes@users.noreply.github.com>
2021-07-07 13:32:37 +0300

    Create kod.js

---
+++ kod.js
@@ -0,0 +1,6 @@
+client.once('ready', () => { //Client hazır olduğunda
+    console.log('ISTEDIGINIZ SEY'); //Konsola "ISTEDIGINIZ SEY" yazdır.
+    client.user.setActivity("BOTUN DURUMUNA YAZILACAK YAZI", { //Bot hesabının aktivitesini "BOTUN DURUMUNA YAZILACAK YAZI" olarak ayarla
+    type: "BOTUN AKTİVİTE DURUMU"//örnek : LISTENING: dinliyor       STREAMING: yayınlıyor      WATCHING: izliyor vb.             NOT: mesela LISTENİNG yazarsanız kod çalışmaz i leri ı olarak yazmanız lazım....
+  })
+  });
